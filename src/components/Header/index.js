d970b8c2d63a35b760e963806e8707b9d90ee60e
Dohyeon Kim <dohyeon.kim@Dohyeons-Mac-mini.local>
2023-08-12 19:46:05 -0500

    Update blog link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import './styles.css'
 const Header = () => (
   <div className="Header">
     <div className="buttons">
-      <RoundedButton title="블로그" link="//blog.dohyeon.com" />
+      <RoundedButton title="블로그" link="//dohyeon.com" />
     </div>
 
     <Container>
